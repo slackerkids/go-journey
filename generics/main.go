349a2bb9b081aec13661094f439f4ac8216c9ef8
slackerkids <145984578+slackerkids@users.noreply.github.com>
2025-10-25 01:11:27 +0500

    feat: generic function add to generics

--- generics/main.go
+++ generics/main.go
@@ -18,6 +18,14 @@ func SumFloats(m map[string]float64) float64 {
 	return s
 }
 
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
 func main() {
 	ints := map[string]int64{
 		"first":  34,
@@ -30,4 +38,7 @@ func main() {
 	}
 
 	fmt.Printf("Non generic sums: %v and %v\n", SumInts(ints), SumFloats(floats))
+	fmt.Printf("Generic Sums: %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
 }
