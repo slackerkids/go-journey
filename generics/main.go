4d18b43a513021cbc7a584e3da6fdeccb6082d19
slackerkids <145984578+slackerkids@users.noreply.github.com>
2025-10-25 01:15:43 +0500

    feat: generic function with interface implement

--- generics/main.go
+++ generics/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type Number interface {
+	int64 | float64
+}
+
 func SumInts(m map[string]int64) int64 {
 	var s int64
 	for _, v := range m {
@@ -26,6 +30,14 @@ func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
 	return s
 }
 
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
 func main() {
 	ints := map[string]int64{
 		"first":  34,
@@ -41,4 +53,5 @@ func main() {
 	fmt.Printf("Generic Sums: %v and %v\n",
 		SumIntsOrFloats(ints),
 		SumIntsOrFloats(floats))
+	fmt.Printf("Generic sums with constraint: %v and %v\n", SumNumbers(ints), SumNumbers(floats))
 }
